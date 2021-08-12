1aea134217e7cd3aa47ed28a2f0de85dcb3de38b
mayorjay <joseolu4gsm@yahoo.com>
2021-08-12 17:43:04 +0100

    feat: Perform multiple network requests concurrently using async

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
@@ -3,6 +3,8 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase3
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
 
 class PerformNetworkRequestsConcurrentlyViewModel(
@@ -27,7 +29,26 @@ class PerformNetworkRequestsConcurrentlyViewModel(
     }
 
     fun performNetworkRequestsConcurrently() {
-        // TODO: Exercise 2
-        // switch to branch "coroutine_course_full" to see solution
+        uiState.value = UiState.Loading
+        val deferred1 = viewModelScope.async {
+            mockApi.getAndroidVersionFeatures(27)
+        }
+
+        val deferred2 = viewModelScope.async {
+            mockApi.getAndroidVersionFeatures(28)
+        }
+
+        val deferred3 = viewModelScope.async {
+            mockApi.getAndroidVersionFeatures(29)
+        }
+
+        viewModelScope.launch {
+            try {
+                val versionFeatures = awaitAll(deferred1, deferred2, deferred3)
+                uiState.value = UiState.Success(versionFeatures)
+            } catch (ex: Exception) {
+                uiState.value = UiState.Error("Network Request failed")
+            }
+        }
     }
 }
