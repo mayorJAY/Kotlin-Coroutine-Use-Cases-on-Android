473cace8a529467d4c995cdf353ed49fb8d6f1fa
mayorjay <joseolu4gsm@yahoo.com>
2021-08-12 16:05:54 +0100

    feat: Perform two sequential network requests

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,14 +1,27 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import java.lang.Exception
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
-        // TODO: Exercise 1
-        // switch to branch "coroutine_course_full" to see solution
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                val recentAndroidVersions = mockApi.getRecentAndroidVersions()
+                val features = mockApi.getAndroidVersionFeatures(recentAndroidVersions.last().apiLevel)
+                uiState.value = UiState.Success(features)
+            } catch (ex: Exception) {
+                Timber.e(ex)
+                uiState.value = UiState.Error("Network Request failed!")
+            }
+        }
     }
 }
